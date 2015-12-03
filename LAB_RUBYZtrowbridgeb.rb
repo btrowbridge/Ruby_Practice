769f4eb0779d9498fd8419b0c9f6a50f336f2a4b
Bradley Trowbridge <bradley.trowbridge@yahoo.com>
2015-12-03 14:09:34 -0500

    I did a ruby class!

---
+++ LAB_RUBYZtrowbridgeb.rb
@@ -0,0 +1,62 @@
+#Bradley Trowbridge
+#COP 3834C-01 Web Application Development
+#Ruby1
+
+class Computer
+
+	def initialize type, speed, on, online
+		@on = on
+		@online = online
+		@speed = speed
+		@type = type
+
+		puts "You made a computer"
+	end
+
+	def turnOn
+		@on = true
+		puts "The computer is on"
+	end
+
+	def turnOff
+		@on = false
+		puts "The computer is off"
+	end
+
+	def getOnline
+		if @on
+			puts "The computer goes online"
+			@online = true
+		else
+			puts "The computer must be on"
+		end
+	end
+
+	def useBrowser
+		if @online
+			puts "You use the browser"
+		else
+			if @on 
+				puts "The computer must be online"
+			else
+				puts "The computer is not on"
+			end
+		end
+	end
+
+	def print
+		puts "Type: "+ @type
+		puts "Speed: " + @speed.to_s + " GHz"
+		puts "Online: " + @online.to_s
+		puts "On: "+ @on.to_s
+	end
+end
+
+myComputer = Computer.new "Acer Aspire", 2.5, false, false
+myComputer.print
+myComputer.useBrowser
+myComputer.turnOn
+myComputer.useBrowser
+myComputer.getOnline
+myComputer.useBrowser 
+myComputer.print
